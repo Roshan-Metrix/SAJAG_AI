2372a644539d01c1a2c9042bc4ded9524a50f035
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-11 01:54:27 +0545

    ogged in user of type rescuers

--- mobileApk/context/AuthContext.jsx
+++ mobileApk/context/AuthContext.jsx
@@ -12,7 +12,9 @@ export const AuthProvider = ({ children }) => {
         });
     }, []);
 
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState({
+        role: "rescuers",
+    });
 
     function saveUser(user) {
         AsyncStorage.setItem("user", JSON.stringify(user)).then(() => {
