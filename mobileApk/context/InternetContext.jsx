c5d90fafaa4259ae1aa50f2d6a7c418f709e5a34
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-14 02:08:20 +0545

    created Internet Provider

---
+++ mobileApk/context/InternetContext.jsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import NetInfo from "@react-native-community/netinfo";
+
+export const InternetContext = createContext();
+
+export const InternetProvider = ({ children }) => {
+    const [isConnected, setIsConnected] = useState(true);
+    const [checked, setChecked] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = NetInfo.addEventListener((state) => {
+            const connected =
+                state.isConnected === true &&
+                state.isInternetReachable !== false;
+
+            setIsConnected(connected);
+            setChecked(true);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <InternetContext.Provider value={{ isConnected, checked }}>
+            {children}
+        </InternetContext.Provider>
+    );
+};
+
+export const useInternet = () => useContext(InternetContext);
