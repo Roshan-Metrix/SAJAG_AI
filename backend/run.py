5e348d23e37022ab17e4b4a216d0f661adc909de
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-06-14 15:28:23 +0545

    fixed bug

--- backend/run.py
+++ backend/run.py
@@ -1,25 +1,25 @@
-import uvicorn
-from app.config.settings import get_settings
-
-if __name__ == "__main__":
-    settings = get_settings()
-    
-    uvicorn.run(
-        "app.main:app",
-        host=settings.ALLOWED_ORIGINS[0].split("://")[1].split(":")[0] if "localhost" in settings.ALLOWED_ORIGINS[0] else "0.0.0.0",
-        port=8000,
-        reload=True,
-        log_level="info"
-    )
-
-
 # import uvicorn
+# from app.config.settings import get_settings
 
 # if __name__ == "__main__":
+#     settings = get_settings()
+    
 #     uvicorn.run(
 #         "app.main:app",
-#         host="0.0.0.0",
+#         host=settings.ALLOWED_ORIGINS[0].split("://")[1].split(":")[0] if "localhost" in settings.ALLOWED_ORIGINS[0] else "0.0.0.0",
 #         port=8000,
 #         reload=True,
-#         log_level="info",
+#         log_level="info"
 #     )
+
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        log_level="info",
+    )
